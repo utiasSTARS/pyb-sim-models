43bdf7132a74fe3d4c6df06f7db97e9c2b326d15
PhilNad <philippe.nadeau1991@gmail.com>
2021-03-18 20:41:19 -0400

    Added directives such that models would be copied to Python libraries data folder upon installation

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     author_email='philippe.nadeau@robotics.utias.utoronto.ca',
     license='MIT',
     packages=['pbsm', 'pbsm.ur5_2f85', 'pbsm.ur5', 'pbsm.Robotiq2f85'],
-    install_requires=['pybullet', 'numpy']
+    install_requires=['pybullet', 'numpy'],
+    include_package_data=True
 )
